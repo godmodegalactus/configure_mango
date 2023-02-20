df84e941a2bc1970d6329078ea3b839e396ec415
Godmode Galactus <godmodegalactus@gmail.com>
2023-02-20 09:15:01 +0100

    deploying programs in the start_test_validator script

--- start_test_validator.sh
+++ start_test_validator.sh
@@ -1,5 +1,7 @@
-solana-test-validator \
---bpf-program BXhdkETgbHrr5QmVBT1xbz3JrMM28u5djbVtmTUfmFTH bin/mango.so \
---bpf-program 3qx9WcNPw4jj3v1kJbWoxSN2ZAakwUXFu9HDr2QjQ6xq bin/serum_dex.so \
---bpf-program 37kmCqYKw41NJxMFVDT5HanZGhwKBQTQJD5hfiYBML7Z bin/pyth_mock.so \
---account MangoCzJ36AjZyKwVj3VnYU4GTonjfVEnJmvvWaxLac bin/mango-mint.json
+solana-test-validator --account MangoCzJ36AjZyKwVj3VnYU4GTonjfVEnJmvvWaxLac bin/mango-mint.json &
+
+sleep 5s
+
+solana program deploy bin/mango.so --program-id bin/mango.json
+solana program deploy bin/serum_dex.so --program-id bin/serum_dex.json
+solana program deploy bin/pyth_mock.so --program-id bin/pyth_mock.json
