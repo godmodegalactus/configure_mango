b77c70944bf00c6c8978905573a922ed32fbe417
Maximilian Schneider <mail@maximilianschneider.net>
2023-03-02 10:54:02 +0100

    make start test validator run on mac os

--- start_test_validator.sh
+++ start_test_validator.sh
@@ -1,11 +1,23 @@
+#!/bin/bash
+
 solana-test-validator --account MangoCzJ36AjZyKwVj3VnYU4GTonjfVEnJmvvWaxLac bin/mango-mint.json &
+pid="$!"
 
-sleep 5s
+# handle ctrl-c
+trap cleanup INT SIGINT 2
 
-solana program deploy bin/mango.so --program-id bin/mango.json
-solana program deploy bin/serum_dex.so --program-id bin/serum_dex.json
-solana program deploy bin/pyth_mock.so --program-id bin/pyth_mock.json
-# idle waiting for abort from user
-( trap exit SIGINT ; read -r -d '' _ </dev/tty ) ## wait for Ctrl-C
+cleanup()
+{
+	echo "cleanup $pid"
+	kill -9 $pid
+}
 
-pkill -9 solana
+sleep 5
+
+solana program deploy bin/mango.so -ul --program-id bin/mango.json
+solana program deploy bin/serum_dex.so -ul --program-id bin/serum_dex.json
+solana program deploy bin/pyth_mock.so -ul --program-id bin/pyth_mock.json
+
+# idle waiting for abort
+wait $pid
+
